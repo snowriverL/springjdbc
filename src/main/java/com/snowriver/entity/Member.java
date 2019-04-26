b5b5bc17efcf5e77a3751bcc71567cb308b26e55
m18210540611@163.com <gfh261022>
2019-04-26 11:01:32 +0800

    first commit

---
+++ src/main/java/com/snowriver/entity/Member.java
@@ -0,0 +1,27 @@
+package com.snowriver.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "t_member")
+@Data
+public class Member implements Serializable {
+    @Id
+    private Long id;
+    private String name;
+    private String addr;
+    private Integer age;
+
+    @Override
+    public String toString() {
+        return "Member{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", addr='" + addr + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
---
+++ src/main/java/com/snowriver/service/JDBCService.java
@@ -0,0 +1,12 @@
+package com.snowriver.service;
+
+import java.util.List;
+
+public class JDBCService {
+
+    private static List<?> select(Object condition) {
+
+        return null;
+    }
+
+}
